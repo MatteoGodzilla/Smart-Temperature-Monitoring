ebb852eb98c83f75545af7eab8f80277068aca56
MatteoGodzilla <catena.matteo90@gmail.com>
2025-01-02 23:31:09 +0100

    simulated retrival of json for the chart history

--- dashboard-frontend/dashboard.js
+++ dashboard-frontend/dashboard.js
@@ -1,25 +1,30 @@
+//chart stuff
 const ctx = document.getElementById("plotCanvas");
-
-const route = "http://localhost:80/"
-
 const chart = new Chart(ctx, {
     type: 'line',
     data: {
         datasets: [{
-            data: [20, 50, 100, 75, 25, 0],
-            label: 'Temperature',
-
-            // This binds the dataset to the left y axis
-            yAxisID: 'temperatureAxis'
-        }, {
-            data: [0.1, 0.25, 0.5, 1.0, 1.0, 0],
             label: 'Window open %',
-
             // This binds the dataset to the right y axis
             yAxisID: 'windowAxis',
             fill:"origin"
+        }, {
+            label: 'Temperature',
+            // This binds the dataset to the left y axis
+            yAxisID: 'temperatureAxis'
+        },{
+            label: 'Temperature Minimum',
+            // This binds the dataset to the left y axis
+            yAxisID: 'temperatureAxis'
+        },{
+            label: 'Temperature Average',
+            // This binds the dataset to the left y axis
+            yAxisID: 'temperatureAxis'
+        },{
+            label: 'Temperature Maximum',
+            // This binds the dataset to the left y axis
+            yAxisID: 'temperatureAxis'
         }],
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
     },
     options: {
         scales: {
@@ -41,18 +46,31 @@ const chart = new Chart(ctx, {
     }
 });
 
+//dashboard functions
+
+const route = "http://localhost:80/"
+
+
 
 async function updateHistory(){
     const url = route + "/history"
-    const json = await fetch(url).then(res => res.json())
-
+    //const json = await fetch(url).then(res => res.json())
+    const json = {"dataPoints": [{"timestamp": 1000, "temperature": 34.55, "window": 0.24}, {"timestamp": 2000, "temperature": 30.45, "window": 0.14}, {"timestamp": 2500, "temperature": 38.5, "window": 0.86}, {"timestamp": 4250, "temperature": 26.76, "window": 0.03}], "minimum": 26.76, "maximum": 38.5, "average": 32.565}
+    for (const dataPoint of json.dataPoints) {
+        chart.data.labels.push(dataPoint.timestamp);
+        chart.data.datasets[0].data.push(dataPoint.window);
+        chart.data.datasets[1].data.push(dataPoint.temperature);
+        chart.data.datasets[2].data.push(json.minimum);
+        chart.data.datasets[3].data.push(json.average);
+        chart.data.datasets[4].data.push(json.maximum);
+    }
+    chart.update();
 
     console.log(json);
 }
-
 
 async function getStatus(){
     const url = route = "/status"
 }
 
-//updateHistory();
+updateHistory();
