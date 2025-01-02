f28cfe2991f81026dc4110e1ba021eeed722949c
MatteoGodzilla <catena.matteo90@gmail.com>
2025-01-02 23:10:03 +0100

    visual improvements on the dashboard

--- dashboard-frontend/README.md
+++ dashboard-frontend/README.md
@@ -1,1 +1,6 @@
-This folder contains the files for the web dashboard interface 
+This folder contains the files for the web dashboard interface
+
+to start the server, use the following command
+```bash
+python -m http.server
+```
--- dashboard-frontend/dashboard.js
+++ dashboard-frontend/dashboard.js
@@ -1,6 +1,8 @@
 const ctx = document.getElementById("plotCanvas");
 
-new Chart(ctx, {
+const route = "http://localhost:80/"
+
+const chart = new Chart(ctx, {
     type: 'line',
     data: {
         datasets: [{
@@ -38,3 +40,19 @@ new Chart(ctx, {
         maintainAspectRatio:false
     }
 });
+
+
+async function updateHistory(){
+    const url = route + "/history"
+    const json = await fetch(url).then(res => res.json())
+
+
+    console.log(json);
+}
+
+
+async function getStatus(){
+    const url = route = "/status"
+}
+
+//updateHistory();
