26edf1d2e9ea719ef8f54aee8fe77c81e947c3e2
MatteoGodzilla <catena.matteo90@gmail.com>
2025-01-07 23:26:42 +0100

    added code to read temperature from sensor

--- temperature-monitoring-subsystem/src/main.cpp
+++ temperature-monitoring-subsystem/src/main.cpp
@@ -1,24 +1,45 @@
 #include <Arduino.h>
 
 #define GREEN_LED 15
-#define RED_LED 4
-#define TEMP_SENSOR 1
+#define RED_LED 2
+#define TEMP_SENSOR 36
+
+#define TEMPERATURE_AVERAGE_SIZE 100
+int readingBuffer[TEMPERATURE_AVERAGE_SIZE] = {};
+int writeIndex = 0;
 
 void setup() {
     pinMode(GREEN_LED, OUTPUT);
     pinMode(RED_LED, OUTPUT);
     pinMode(TEMP_SENSOR, INPUT);
     Serial.begin(115200);
+    //connect to mqtt
+}
+
+void pushReading(){
+    //note that the ADC in esp32 is not perfectly linear, but we'll pretent it is
+    readingBuffer[writeIndex] = analogRead(TEMP_SENSOR);
+    writeIndex = (writeIndex + 1) % TEMPERATURE_AVERAGE_SIZE;
+}
+
+//the ADC is very, VERY noisy, so we take an average of the last N samples and use that as a value
+int getAverageReading(){
+    long sum = 0;
+    for(int i = 0; i < TEMPERATURE_AVERAGE_SIZE; i++){
+        sum += readingBuffer[i];
+    }
+    return sum / TEMPERATURE_AVERAGE_SIZE;
 }
 
 void loop() {
-    if(Serial){
-        digitalWrite(GREEN_LED, HIGH);
-        digitalWrite(RED_LED, LOW);
-        Serial.println("aaaa");
-        Serial.flush();
-    } else {
-        digitalWrite(GREEN_LED, LOW);
-        digitalWrite(RED_LED, HIGH);
-    }
+    pushReading();
+    //We're using milliVolts because if we store as Volts then we have too much error from floating point
+    float voltagemV = (float)getAverageReading() * 3300 / 4095;
+    //TMP36 provides a 750mV output at 25°C
+    //it has an output scale of 10mV / °C
+    float temperature = (voltagemV - 750) / 10 + 25.0;
+
+    Serial.print(voltagemV);
+    Serial.print("\t|");
+    Serial.println(temperature);
 }
