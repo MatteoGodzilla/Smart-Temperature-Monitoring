f0fdbe68726585b3814eafb0aa787b9f949a4633
MatteoGodzilla <catena.matteo90@gmail.com>
2025-01-08 11:24:34 +0100

    better temperature reading using calibrated adc1

--- temperature-monitoring-subsystem/src/main.cpp
+++ temperature-monitoring-subsystem/src/main.cpp
@@ -1,30 +1,41 @@
 #include <Arduino.h>
+#include "esp_adc_cal.h"
 
 #define GREEN_LED 15
 #define RED_LED 2
-#define TEMP_SENSOR 36
+//TEMP_SENSOR_GPIO AND TEMP_SENSOR_ADC_CHANNEL MUST REFER TO THE SAME
+#define TEMP_SENSOR_GPIO 36
+#define TEMP_SENSOR_ADC_CHANNEL ADC_CHANNEL_0
+//the code is written to use ADC1, because we also have to use the wifi module, which conflicts with ADC2
+
+esp_adc_cal_characteristics_t adcCurve;
 
 #define TEMPERATURE_AVERAGE_SIZE 100
-int readingBuffer[TEMPERATURE_AVERAGE_SIZE] = {};
+uint32_t readingBuffer[TEMPERATURE_AVERAGE_SIZE] = {};
 int writeIndex = 0;
 
 void setup() {
     pinMode(GREEN_LED, OUTPUT);
     pinMode(RED_LED, OUTPUT);
-    pinMode(TEMP_SENSOR, INPUT);
+    pinMode(TEMP_SENSOR_GPIO, INPUT);
     Serial.begin(115200);
+    //setup adc curve
+    analogSetPinAttenuation(TEMP_SENSOR_GPIO, ADC_11db);
+    esp_adc_cal_value_t returnType = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_12, ESP_ADC_CAL_VAL_EFUSE_VREF, &adcCurve);
     //connect to mqtt
 }
 
 void pushReading(){
     //note that the ADC in esp32 is not perfectly linear, but we'll pretent it is
-    readingBuffer[writeIndex] = analogRead(TEMP_SENSOR);
+    uint32_t voltagemV;
+    esp_adc_cal_get_voltage(TEMP_SENSOR_ADC_CHANNEL, &adcCurve, &voltagemV);
+    readingBuffer[writeIndex] = voltagemV;
     writeIndex = (writeIndex + 1) % TEMPERATURE_AVERAGE_SIZE;
 }
 
 //the ADC is very, VERY noisy, so we take an average of the last N samples and use that as a value
-int getAverageReading(){
-    long sum = 0;
+double getAverageReading(){
+    double sum = 0;
     for(int i = 0; i < TEMPERATURE_AVERAGE_SIZE; i++){
         sum += readingBuffer[i];
     }
@@ -33,11 +44,10 @@ int getAverageReading(){
 
 void loop() {
     pushReading();
-    //We're using milliVolts because if we store as Volts then we have too much error from floating point
-    float voltagemV = (float)getAverageReading() * 3300 / 4095;
+    double voltagemV = getAverageReading();
     //TMP36 provides a 750mV output at 25°C
     //it has an output scale of 10mV / °C
-    float temperature = (voltagemV - 750) / 10 + 25.0;
+    double temperature = (voltagemV - 750.0) / 10 + 25.0;
 
     Serial.print(voltagemV);
     Serial.print("\t|");
